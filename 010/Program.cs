a81233bfbe43ca7d6dcc6a35c7cc45affeb45dd6
zayarniy <zaazaa@yandex.ru>
2022-11-15 19:08:03 +0300

    Update Program.cs

--- 010/Program.cs
+++ 010/Program.cs
@@ -1,4 +1,3 @@
 ﻿// 10. Вывести на экран последнюю цифру целого числа введенного с клавиатуры. 
 int n=Convert.ToInt32(Console.ReadLine());
-for (n=n%10;n>=9;);
-Console.WriteLine("Последняя цифра: " +n);
+Console.WriteLine("Последняя цифра: " +n%10);
